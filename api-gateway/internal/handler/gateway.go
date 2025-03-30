71ad1f3882bcba656d92e88cbfe1767a7558257f
Rangdog <thanhhaxuan02@gmail.com>
2025-03-30 22:44:36 +0700

    fix dns api-gate-way

--- api-gateway/internal/handler/gateway.go
+++ api-gateway/internal/handler/gateway.go
@@ -33,8 +33,8 @@ func (h *GatewayHandler) ProxyToService(serviceName string) gin.HandlerFunc{
 		// 🛑 Log địa chỉ IP để debug
 		fmt.Printf("[DEBUG] Found service %s at IP: %s, Port: %d\n", serviceName, ip, port)
 		// targetURL := fmt.Sprintf("http://%s:%d", service.Service.Address, service.Service.Port) bỏ port
-		targetURL := fmt.Sprintf("http://%s%s", service.Service.Address, c.Request.URL.Path) // Giữ nguyên đường dẫn gốc
-		
+		// targetURL := fmt.Sprintf("http://%s%s", service.Service.Address, c.Request.URL.Path) // Giữ nguyên đường dẫn gốc
+		targetURL := fmt.Sprintf("http://%s.default.svc.cluster.local:%s", ip, c.Request.URL.Path)
 		// 🛑 Log URL để debug
 		fmt.Printf("[DEBUG] Proxy request to: %s\n", targetURL)
 
