baedf4171bccdf98453a01faceafa2ccb2d0e6b6
Rangdog <thanhhaxuan02@gmail.com>
2025-04-01 12:59:00 +0700

    fix input userservice

--- user-service/api/handlers/user_handler.go
+++ user-service/api/handlers/user_handler.go
@@ -27,7 +27,7 @@ func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request){
 		Email string `json:"email" validate:"required, email"`
 		Password string `json:"password" validate:"required, min 6"`
 	}
-
+	fmt.Printf(input.Email,input.Password)
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil{
 		response.Error(w,http.StatusBadRequest, err)
 		return
