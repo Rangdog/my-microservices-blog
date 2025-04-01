b3729db85c834eaf227e1a6355a2e773da776b7c
Rangdog <thanhhaxuan02@gmail.com>
2025-04-01 13:47:24 +0700

    add errors

--- user-service/api/handlers/user_handler.go
+++ user-service/api/handlers/user_handler.go
@@ -23,7 +23,7 @@ func NewUserHandler(service *service.UserService) *UserHandler{
 func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request){
 	var input struct{
 		Email string `json:"email" validate:"required,email"`
-		Password string `json:"password" validate:"required,min 6"`
+		Password string `json:"password" validate:"required,min=6"`
 	}
 	fmt.Printf("[DEBUG] body: %s",r.Body)
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil{
